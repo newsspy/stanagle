01898045022827d7fc961bb99feb06ebf7a169c6
newsspy <158444288+newsspy@users.noreply.github.com>
2024-02-20 15:21:50 +0530

    Update env.js

--- public/js/env.js
+++ public/js/env.js
@@ -1,2 +1,2 @@
-echo 'export const WEBSOCKET_URL = "ws://stanagle.onrender.com:8080"' >> public/js/env.js
-export SERVER_PORT=8080
+'echo 'export const WEBSOCKET_URL = "ws://stanagle.onrender.com:8080"' >> public/js/env.js
+export SERVER_PORT=8080;
